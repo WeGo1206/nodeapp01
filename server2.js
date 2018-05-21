95d84b22f231152b97c42953636d60a21a0af867
WeGo1206 <gweisflog@hotmail.com>
2018-05-21 21:06:30 +0200

    Dynamischer Dateiname

--- server2.js
+++ server2.js
@@ -1,19 +1,25 @@
 var http = require('http'),
     fileSystem = require('fs'),
-    path = require('path');
+    path = require('path'),
+    moment = require('moment');
 
 const hostname = '192.168.1.106';
 const port = 3000;
+var requestCounter = 0;
 
 const server = http.createServer(function(request, response) {
-    var filePath = path.join('/home/pi/Documents', 'Temperatur_Wohnung_20180521.txt');
+    requestCounter++;
+    var date = moment().format('YYYYMMDD');
+    var fileName = 'Temperatur_Wohnung_' + date + '.txt'
+    console.log('Requested: ' + fileName + ' [' + requestCounter + ']');	
+    var filePath = path.join('/home/pi/Documents', fileName);
     var stat = fileSystem.statSync(filePath);
 
     response.writeHead(200, {
         'Content-Type': 'text/txt',
         'Content-Length': stat.size
     });
-
+    		
     var readStream = fileSystem.createReadStream(filePath);
     readStream.pipe(response);
 });
