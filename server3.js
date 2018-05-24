6898f31f213d4cb3fb8f8a5f1f15a2ad55530cbb
WeGo1206 <gweisflog@hotmail.com>
2018-05-24 22:58:35 +0200

    Temperatur Chart

--- server3.js
+++ server3.js
@@ -8,19 +8,67 @@ const port = 3000;
 var requestCounter = 0;
 var htmlFile;
 
-fs.readFile('./view/line.html', function(err, data) {
-	if(err) {
-		throw err;
-	}
-	htmlFile = data;
-});
-
 const server = http.createServer(function(request, response) {
 	requestCounter++;
-        console.log('Requested: ' + ' [' + requestCounter + ']');	
+	var date = moment().format('YYYYMMDD');
+    	var fileName = '/home/pi/Documents/Temperatur_Wohnung_' + date + '.txt';
+	      
+
+	console.log('Requested: ' + ' [' + requestCounter + ']');
+
+	var data = fs.readFileSync(fileName, 'utf-8');
+	var htmlFile = fs.readFileSync('./TemplateChart.html', 'utf-8');
+
+	console.log(data);
+	console.log(htmlFile);
+
+	var time = [];
+	var dataset1 = [];
+	var dataset2 = [];
+
+	var split = data.split('\n')
+	console.log(split);
+	var split2 = [];
+	var normArr = [];
+	var i = 0;
+	for (i = 0; i < split.length; i++) {
+    		split2 = split[i].split(';');
+    		normArr.push(split2);
+    		console.log(split2);
+	};
+
+console.log(normArr);
+
+i = 0;
+for (i = 0; i < normArr.length; i++) {
+    if(normArr[i][3]) {
+        time.push("\"" + normArr[i][3] + "\"");
+    };
+    if(normArr[i][0]) {
+        dataset1.push(normArr[i][0].replace(",","."));
+    };
+    if(normArr[i][0]) {
+        dataset2.push(normArr[i][1].replace(",","."));
+    };
+};
+console.log(time);
+console.log(dataset1);
+console.log(dataset2);
+
+
+var newHtmlFile;
+newHtmlFile = htmlFile.replace("@xvalues", time);
+newHtmlFile = newHtmlFile.replace("@yvalues1", dataset1);
+newHtmlFile = newHtmlFile.replace("@yvalues2", dataset2);
+console.log(newHtmlFile);
+
+var fs2 = fs.writeFileSync('./TempChart.html', newHtmlFile, 'utf-8');
+
+
+	
         response.writeHead(200, {
         'Content-Type': 'text/html'});
-	response.write(htmlFile);
+	response.write(newHtmlFile);
 	response.end();
 });
 
