595e8db17a725ebc2f1bbbbe88902217ef31c2a5
WeGo1206 <gweisflog@hotmail.com>
2018-05-25 17:44:25 +0200

    V0.0.3 Beschriftungen

--- server3.js
+++ server3.js
@@ -24,25 +24,25 @@ const server = http.createServer(function(request, response) {
 	}
 	var htmlFile = fs.readFileSync('./TemplateChart.html', 'utf-8');
 
-	console.log(data);
-	console.log(htmlFile);
+	//console.log(data);
+	//console.log(htmlFile);
 
 	var time = [];
 	var dataset1 = [];
 	var dataset2 = [];
 
 	var split = data.split('\n')
-	console.log(split);
+	//console.log(split);
 	var split2 = [];
 	var normArr = [];
 	var i = 0;
 	for (i = 0; i < split.length; i++) {
     		split2 = split[i].split(';');
     		normArr.push(split2);
-    		console.log(split2);
+    		//console.log(split2);
 	};
 
-console.log(normArr);
+//console.log(normArr);
 
 i = 0;
 for (i = 0; i < normArr.length; i++) {
@@ -56,16 +56,16 @@ for (i = 0; i < normArr.length; i++) {
         dataset2.push(normArr[i][1].replace(",","."));
     };
 };
-console.log(time);
-console.log(dataset1);
-console.log(dataset2);
+//console.log(time);
+//console.log(dataset1);
+//console.log(dataset2);
 
 
 var newHtmlFile;
 newHtmlFile = htmlFile.replace("@xvalues", time);
 newHtmlFile = newHtmlFile.replace("@yvalues1", dataset1);
 newHtmlFile = newHtmlFile.replace("@yvalues2", dataset2);
-console.log(newHtmlFile);
+//console.log(newHtmlFile);
 
 var fs2 = fs.writeFileSync('./TempChart.html', newHtmlFile, 'utf-8');
 
