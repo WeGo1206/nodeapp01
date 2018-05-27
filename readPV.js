820be6733775564549b5d5a9b1a83ba351a3729f
WeGo1206 <gweissflog@hotmail.com>
2018-05-27 21:04:11 +0200

    new Feature: Preselect TimeRange

--- readPV.js
+++ readPV.js
@@ -4,8 +4,23 @@ var moment = require('moment');
 module.exports = {
     readPV: function(timeRangeDays) {
         var date = moment().subtract(timeRangeDays, 'days').format("YYYYMMDD");
-        console.log(date);
-        var data = fs.readFileSync('./TestData/Temperatur_Wohnung_20180522.txt', 'utf-8');
+        //console.log(date);
+        var fileName = [];
+        var d;
+        var dataTemp = "";
+        var data = "";
+        for (d = 1; d <= timeRangeDays; d++) {
+
+            fileName.push('Temperatur_Wohnung_' + moment().subtract(timeRangeDays-d,"days").format('YYYYMMDD'));
+        };
+        console.log(fileName);
+        d = 0;
+        for (d = 0; d < fileName.length; d++) {
+            dataTemp = fs.readFileSync('./TestData/' + fileName[d] + '.txt', 'utf-8');
+            data = data.concat(dataTemp);
+        };
+        console.log(data);
+        
         var htmlFile = fs.readFileSync('./TemplateChart.html', 'utf-8');
         //console.log(data);
         //console.log(htmlFile);
@@ -30,6 +45,7 @@ module.exports = {
         i = 0;
         for (i = 0; i < normArr.length; i++) {
             if(normArr[i][3]) {
+                normArr[i][3] = normArr[i][3].substring(0,19);
                 time.push("\"" + normArr[i][3] + "\"");
             };
             if(normArr[i][0]) {
@@ -39,9 +55,10 @@ module.exports = {
                 dataset2.push(normArr[i][1].replace(",","."));
             };
         };
-        console.log(time);
-        console.log(dataset1);
-        console.log(dataset2);
+       
+        //console.log(time);
+        //console.log(dataset1);
+        //console.log(dataset2);
         
         var newHtmlFile;
         newHtmlFile = htmlFile.replace("@xvalues", time);
--- server004.js
+++ server004.js
@@ -5,23 +5,45 @@ var http = require('http'),
     express = require('express'),
     readPV = require('./readPV');
 
-const hostname = '192.168.1.106';
-const port = 80;
+const hostname = '192.168.1.114';
+const port = 3000;
 var requestCounter = 0;
 var htmlFile;
 var error;
-console.log(readPV.readPV(10));
+//console.log(readPV.readPV(6));
 
-/* const app = express();
-
-
+const app = express();
 
 app.get('/', function (req, res) {
-res.send('Chart Temperatur-Messung mit Raspberry PI und DS18B20 Sensor \n WeGo v0.0.4')});
+res.send(readPV.readPV(1))});
+
+app.get('/Heute', function (req, res) {
+  res.send(readPV.readPV(1))});
+
+app.get('/2t', function (req, res) {
+  res.send(readPV.readPV(2))});
+
+app.get('/3t', function (req, res) {
+  res.send(readPV.readPV(3))});
+
+app.get('/5t', function (req, res) {
+  res.send(readPV.readPV(5))});
+
+app.post('/2t', function(req, res){   
+  res.redirect('/2t');});
+
+app.post('/Heute', function(req, res){   
+  res.redirect('/Heute');});
+
+app.post('/3t', function(req, res){   
+  res.redirect('/3t');});
+
+app.post('/5t', function(req, res){   
+  res.redirect('/5t');});
 
 app.get('/About', function (req, res) {
 res.send('Temperatur-Messung mit Raspberry PI und DS18B20 Sensor \n WeGo v0.0.4')});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); */
+});
