6b83b1582d85fb7c114b504a127713b83a80d27a
WeGo1206 <gweissflog@hotmail.com>
2018-06-03 19:29:32 +0200

    New feature: manual time range

--- readByDateInput.js
+++ readByDateInput.js
@@ -8,17 +8,18 @@ module.exports = {
         //console.log(date);
         var fileName = [];
         var d;
-        var i;
         var dataTemp = "";
         var data = "";
         var splitByRow;
         var arrayOfTime = [];
         var arrayOfYvalue1 = [];
         var arrayOfYvalue2 = [];
+        var arrayOfYvalue3 = [];
         var splitByValue = [];
         var arrayOfSeparatedValues = [];
         var avgOfYvalue1 = 0;
         var avgOfYvalue2 = 0;
+        var avgOfYvalue3 = 0;
         var htmlTemplateFile;
         var responseHTML;
 
@@ -61,44 +62,56 @@ module.exports = {
                 arrayOfTime.push("\"" + arrayOfSeparatedValues[i][3] + "\"");
             };
             if(arrayOfSeparatedValues[i][0]) {
-                arrayOfYvalue1.push(Number(arrayOfSeparatedValues[i][0].replace(",",".")));
+		                arrayOfYvalue1.push(Number(arrayOfSeparatedValues[i][0].replace(",",".")));
 		
             };
-            if(arrayOfSeparatedValues[i][0]) {
+            if(arrayOfSeparatedValues[i][1]) {
                 arrayOfYvalue2.push(Number(arrayOfSeparatedValues[i][1].replace(",",".")));
+		
+            };
+            if(arrayOfSeparatedValues[i][2]) {
+                arrayOfYvalue3.push(Number(arrayOfSeparatedValues[i][1].replace(",",".")));
 		
             };
         };
        
-        //console.log(time);
+	    //console.log(time);
         //console.log(dataset1);
         //console.log(dataset2);
 
         for(i=0; i< arrayOfYvalue1.length; i++) {
 		avgOfYvalue1 += arrayOfYvalue1[i];
-    };
+        };
     
-	avgOfYvalue1 = avgOfYvalue1/arrayOfYvalue1.length;
+	    avgOfYvalue1 = avgOfYvalue1/arrayOfYvalue1.length;
  
-	for(i=0; i< arrayOfYvalue2.length; i++) {
-		avgOfYvalue2 += arrayOfYvalue2[i];
-	};
-	avgOfYvalue2 = avgOfYvalue2/arrayOfYvalue2.length;
+	    for(i=0; i< arrayOfYvalue2.length; i++) {
+		    avgOfYvalue2 += arrayOfYvalue2[i];
+	    };
+        avgOfYvalue2 = avgOfYvalue2/arrayOfYvalue2.length;
+        
+        for(i=0; i< arrayOfYvalue3.length; i++) {
+		    avgOfYvalue3 += arrayOfYvalue3[i];
+	    };
+	    avgOfYvalue3 = avgOfYvalue3/arrayOfYvalue3.length;
 
         responseHTML = htmlTemplateFile.replace("@xvalues", arrayOfTime);
         responseHTML = responseHTML.replace("@yvalues1", arrayOfYvalue1);
         responseHTML = responseHTML.replace("@yvalues2", arrayOfYvalue2);
+        responseHTML = responseHTML.replace("@yvalues3", arrayOfYvalue3);
         responseHTML = responseHTML.replace("@num1", arrayOfYvalue1.length);
         responseHTML = responseHTML.replace("@num2", arrayOfYvalue2.length);
+        responseHTML = responseHTML.replace("@num3", arrayOfYvalue3.length);
         responseHTML = responseHTML.replace("@min1", Math.min(...arrayOfYvalue1));
         responseHTML = responseHTML.replace("@min2", Math.min(...arrayOfYvalue2));
+        responseHTML = responseHTML.replace("@min3", Math.min(...arrayOfYvalue3));
         responseHTML = responseHTML.replace("@max1", Math.max(...arrayOfYvalue1));
         responseHTML = responseHTML.replace("@max2", Math.max(...arrayOfYvalue2));
+        responseHTML = responseHTML.replace("@max3", Math.max(...arrayOfYvalue3));
         responseHTML = responseHTML.replace("@avg1", avgOfYvalue1.toFixed(2));
         responseHTML = responseHTML.replace("@avg2", avgOfYvalue2.toFixed(2));
-        responseHTML = responseHTML.replace("id=\"@sDate\"", "id=\"@sDate\"" + "value=\"" + moment(sdate).format("YYYY-MM-DD") + "\"");
-        responseHTML = responseHTML.replace("id=\"@eDate\"", "id=\"@eDate\"" + "value=\"" + moment(edate).format("YYYY-MM-DD") + "\"");
-        //console.log(moment(sdate).format("YYYY-MM-DD"));
+        responseHTML = responseHTML.replace("@avg3", avgOfYvalue3.toFixed(2));
+        //console.log(responseHTML);
         return responseHTML;
     }
 }
