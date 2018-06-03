9d37a63fa0490cd579c83ca8f0d6e591d2e70542
WeGo1206 <gweissflog@hotmail.com>
2018-06-03 15:55:40 +0200

    New feature: manual time range

---
+++ readByDateInput.js
@@ -0,0 +1,105 @@
+var fs = require('fs');
+var moment = require('moment');
+var path    = require("path");
+
+module.exports = {
+    readByDateInput: function(sdate, edate) {
+        //var date = moment().subtract(timeRangeByDays, 'days').format("YYYYMMDD");
+        //console.log(date);
+        var fileName = [];
+        var d;
+        var i;
+        var dataTemp = "";
+        var data = "";
+        var splitByRow;
+        var arrayOfTime = [];
+        var arrayOfYvalue1 = [];
+        var arrayOfYvalue2 = [];
+        var splitByValue = [];
+        var arrayOfSeparatedValues = [];
+        var avgOfYvalue1 = 0;
+        var avgOfYvalue2 = 0;
+        var htmlTemplateFile;
+        var responseHTML;
+
+        for (d = sdate; d <= edate; d++) {
+
+            fileName.push('Temperatur_Wohnung_' + d);
+        };
+        //console.log(fileName);
+        //d = 0;
+        for (d = 0; d < fileName.length; d++) {
+	    try{
+               dataTemp = fs.readFileSync('/home/pi/Documents/' + fileName[d] + '.txt', 'utf-8');
+               data = data.concat(dataTemp);
+	    }
+	    catch(err){
+               console.log('./' + fileName[d] + '.txt');
+	    };
+        };
+        //console.log(data);
+        
+        htmlTemplateFile = fs.readFileSync('./TemplateChart.html', 'utf-8');
+        //console.log(data);
+        //console.log(htmlFile);
+               
+        splitByRow = data.split('\n')
+        //console.log(split);
+        //var i = 0;
+        for (i = 0; i < splitByRow.length; i++) {
+            splitByValue = splitByRow[i].split(';');
+            arrayOfSeparatedValues.push(splitByValue);
+            //console.log(split2);
+        };
+        
+        //console.log(normArr);
+
+       // i = 0;
+        for (i = 0; i < arrayOfSeparatedValues.length; i++) {
+            if(arrayOfSeparatedValues[i][3]) {
+                arrayOfSeparatedValues[i][3] = arrayOfSeparatedValues[i][3].substring(0,19);
+                arrayOfTime.push("\"" + arrayOfSeparatedValues[i][3] + "\"");
+            };
+            if(arrayOfSeparatedValues[i][0]) {
+                arrayOfYvalue1.push(Number(arrayOfSeparatedValues[i][0].replace(",",".")));
+		
+            };
+            if(arrayOfSeparatedValues[i][0]) {
+                arrayOfYvalue2.push(Number(arrayOfSeparatedValues[i][1].replace(",",".")));
+		
+            };
+        };
+       
+        //console.log(time);
+        //console.log(dataset1);
+        //console.log(dataset2);
+
+        for(i=0; i< arrayOfYvalue1.length; i++) {
+		avgOfYvalue1 += arrayOfYvalue1[i];
+    };
+    
+	avgOfYvalue1 = avgOfYvalue1/arrayOfYvalue1.length;
+ 
+	for(i=0; i< arrayOfYvalue2.length; i++) {
+		avgOfYvalue2 += arrayOfYvalue2[i];
+	};
+	avgOfYvalue2 = avgOfYvalue2/arrayOfYvalue2.length;
+
+        responseHTML = htmlTemplateFile.replace("@xvalues", arrayOfTime);
+        responseHTML = responseHTML.replace("@yvalues1", arrayOfYvalue1);
+        responseHTML = responseHTML.replace("@yvalues2", arrayOfYvalue2);
+        responseHTML = responseHTML.replace("@num1", arrayOfYvalue1.length);
+        responseHTML = responseHTML.replace("@num2", arrayOfYvalue2.length);
+        responseHTML = responseHTML.replace("@min1", Math.min(...arrayOfYvalue1));
+        responseHTML = responseHTML.replace("@min2", Math.min(...arrayOfYvalue2));
+        responseHTML = responseHTML.replace("@max1", Math.max(...arrayOfYvalue1));
+        responseHTML = responseHTML.replace("@max2", Math.max(...arrayOfYvalue2));
+        responseHTML = responseHTML.replace("@avg1", avgOfYvalue1.toFixed(2));
+        responseHTML = responseHTML.replace("@avg2", avgOfYvalue2.toFixed(2));
+        responseHTML = responseHTML.replace("id=\"@sDate\"", "id=\"@sDate\"" + "value=\"" + moment(sdate).format("YYYY-MM-DD") + "\"");
+        responseHTML = responseHTML.replace("id=\"@eDate\"", "id=\"@eDate\"" + "value=\"" + moment(edate).format("YYYY-MM-DD") + "\"");
+        //console.log(moment(sdate).format("YYYY-MM-DD"));
+        return responseHTML;
+    }
+}
+
---
+++ server006.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path    = require("path");
+var bodyParser = require('body-parser');
+var piFixedValue = require('./readPV');
+var piSelectValue = require('./readByDateInput');
+
+var app = express();
+
+const hostname = '192.168.1.106';
+const port = 80;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', function (req, res) {
+  //this corresponds to URL query parameters after '?' combined with '&'
+  //console.log('get2: ' + req.query.sdate);
+  //console.log('get2: ' + req.query.edate);
+  
+  //this corresponds to route parameters directly the path separeted by '/'
+  console.log('get2: ' + req.params.sdate);
+  console.log('get2: ' + req.params.edate);
+  res.send(piFixedValue.readPV(8));
+});
+
+app.post('/pi', function(req, res){ 
+  console.log('post: ' + req.body.sdate);
+  console.log('post: ' + req.body.edate);
+  //this corrsponds to req.query.xxxx in get route
+  //res.redirect('/page/1/?sdate='+ req.body.sdate+ '&edate='+ req.body.edate);
+
+  //this corresponds to req.params.xxxx in get route
+  //res.redirect('/page/1/'+ req.body.sdate+ '/'+ req.body.edate);
+  var sDateF = req.body.sdate;
+  var eDateF = req.body.edate;
+  sDateF = sDateF.replace(/-/g,"");
+  eDateF = eDateF.replace(/-/g,"");  
+  res.send(piSelectValue.readByDateInput(sDateF, eDateF));
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
