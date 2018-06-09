b313744411cf838cd130b6d7493c5959e22b7de0
WeGo1206 <gweissflog@hotmail.com>
2018-06-09 18:06:16 +0200

    styles in separated css file

--- server007.js
+++ server007.js
@@ -11,6 +11,7 @@ const port = 80;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   //this corresponds to URL query parameters after '?' combined with '&'
