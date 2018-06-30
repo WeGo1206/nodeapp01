6700310e7eedd989753b7c72aa40d0e3ae015572
WeGo1206 <gweissflog@hotmail.com>
2018-06-30 22:16:32 +0200

    Menue Documentation

--- Dev/GetTempV1.py
+++ Dev/GetTempV1.py
@@ -1,50 +1,30 @@
-# tempread.py
-import smtplib, sys
+import sys
 import os, time
-from email.mime.text import MIMEText
-from email.header import Header
 from time import *
-
-os.system("modprobe wire") #1
+os.system("modprobe wire")
 os.system("modprobe w1-gpio")
 os.system("modprobe w1-therm")
 
-##for d in os.listdir("/sys/bus/w1/devices"): #2
-##    if d.startswith("10") or d.startswith("28"): #3
-##        deviceFile = "/sys/bus/w1/devices/" + d + "/w1_slave"
-##def readTemp():
-##    ok = False
-##    while not ok:
-##        f = open(deviceFile, "r") #4
-##        first, second = f.readlines() #5
-##        f.close()
-##        if first.find("YES") != -1: #6
-##            ok = True
-##        tempString = second.split("=")[1] #7
-##        return int(tempString)/1000 #8
-
-import os, time
-
 ok = False
 while not ok:
-    f1 = open("/sys/bus/w1/devices/28-000007360dfe/w1_slave", "r") #4
-    first1, second1 = f1.readlines() #5
+    f1 = open("/sys/bus/w1/devices/28-000007360dfe/w1_slave", "r")
+    first1, second1 = f1.readlines()
     f1.close()
-    if first1.find("YES") != -1: #6
+    if first1.find("YES") != -1:
         ok = True
-    tempString1 = second1.split("=")[1] #7
-T1 = int(tempString1)/1000 #8
+    tempString1 = second1.split("=")[1] 
+T1 = int(tempString1)/1000
 print("T1=",T1)
 
 ok = False
 while not ok:
-    f2 = open("/sys/bus/w1/devices/28-00000735e872/w1_slave", "r") #4
-    first2, second2 = f2.readlines() #5
+    f2 = open("/sys/bus/w1/devices/28-00000735e872/w1_slave", "r")
+    first2, second2 = f2.readlines()
     f2.close()
-    if first2.find("YES") != -1: #6
+    if first2.find("YES") != -1:
         ok = True
-    tempString2 = second2.split("=")[1] #7
-T2 = int(tempString2)/1000 #8
+    tempString2 = second2.split("=")[1]
+T2 = int(tempString2)/1000
 print("T2=",T2)
 
 tFile = open("/sys/class/thermal/thermal_zone0/temp")
@@ -53,13 +33,9 @@ tFile.close()
 fcpuTemp = round(float(cpuTemp)/1000,1)
 
 localtime = time.strftime("%d.%m.%Y %H:%M:%S")
-
 print(T1,"°C /", T2,"°C /",localtime)
-##logText = "Raum= " + str(round(T2,1)) + "°C" + " / Aussen= " + str(round(T1,1)) + "°C" + " / CPU= " + str(fcpuTemp) + "°C / " + localtime + "\n"
-##macht komische Zeichen vor °C
 logText = str(round(T1,1)) + ";" + str(round(T2,1)) + ";" + str(fcpuTemp) + ";" + localtime + "\n"
 logText = logText.replace(".",",",3)
-
 filename_daily= time.strftime("/home/pi/Documents/Temperatur_Wohnung_%Y%m%d.txt")
 fT = open(filename_daily, "a")
 fT.write(logText)
