6179ae0cc065a5eee2b91dd6069c33900eeb3651
WeGo1206 <gweisflog@hotmail.com>
2018-07-08 09:41:15 +0200

    System Info

---
+++ Dev/GetSysInfo.py
@@ -0,0 +1,32 @@
+import sys
+import os, time
+from time import *
+
+result=os.statvfs('/')
+blocksize=result.f_frsize
+totalblocks=result.f_blocks
+freeblocks=result.f_bfree
+giga=1024*1024*1024
+totalsize=totalblocks*blocksize/giga
+freesize=freeblocks*blocksize/giga
+usedsize=totalsize-freesize
+print('total disk size= %s' % totalsize)
+print('used disk size= %s' % usedsize)
+print('free disk size= %s' % freesize)
+
+
+lines = os.popen('free -m').readlines()[-2]
+print('readlines %s' % lines)
+usedmem, freemem = map(int, os.popen('free -m').readlines()[-2].split()[2:])
+totmem = usedmem + freemem
+print('used memory (RAM)= %s' % usedmem)
+print('free memory (RAM)= %s' % freemem)
+print('total memory (RAM)= %s' % totmem)
+
+                
+logText = str(round(totalsize,1)) + ";" + str(round(usedsize,1)) + ";" + str(round(freesize,1)) + ";" + str(round(totmem,0)) + ";" + str(round(usedmem,0)) + ";" + str(round(freemem,0)) + "\n"
+print(logText)
+
+fT = open("/home/pi/Documents/PiSysInfo.txt", "w")
+fT.write(logText)
+fT.close
