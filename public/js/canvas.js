5fee864c792d98503c24a36bcdc6d77583dc7bb7
WeGo1206 <gweissflog@hotmail.com>
2018-07-25 19:00:46 +0200

    canvas

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -2,5 +2,30 @@ var canvas = document.getElementById('animation-canvas');
 var c = canvas.getContext('2d');
 
 console.log('start canvas')
-c.fillRect(100, 100, 100, 100);
+// c.fillRect(100, 100, 100, 100);
+
+
+
+var x = 20;
+var y = 20;
+var dx = 4;
+var dy = 4;
+var radius = 30;
+
+function animate() {
+    requestAnimationFrame(animate) {
+        c.clearRect(0,0,innerWidth,innerHeight);
+        c.beginPath();
+        c.arc(x,y,radius,0,Math.PI * 2, false);
+        c.strokeStyle = 'blue';
+        c.stroke();
+
+        if ( x + radius > innerWidth || y - radius < 0) {
+            dx = -dx;
+        }
+        if ( x + radius > innerHeight || y - radius < 0) {
+            dy = -dy;
+        }
+    }
+}
 console.log('end canvas')
