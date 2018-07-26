f38267b99fe28f3b0233a2b8000c0abe002cd0d5
WeGo1206 <gweissflog@hotmail.com>
2018-07-26 21:31:09 +0200

    canvas

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -66,7 +66,7 @@ function animate() {
     //     console.log(x2);
     // }
     if ( y2 + radius2 > 150 || y2 - radius2 < 0) {
-        dy2 = (dy2 * -0.999);
+        dy2 = (dy2 * -1);
         console.log(y2);
     }
     else {
