78474d939cc1f8af87c3212501acd83d490dffee
WeGo1206 <gweissflog@hotmail.com>
2018-07-28 21:44:13 +0200

    canvas

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -106,9 +106,8 @@ function animate() {
         dy2 += 1;
         //console.log(dy2);
     }
-   if ( dy2 = 0)  {
-    x2 = x2 + (dx2 * 0.5);
-    console.log("grounded");
+   if ( dy2 === 0)  {
+    x2 = x2 + (dx2 * 0.8);
    }
    else {
     x2 += dx2;
