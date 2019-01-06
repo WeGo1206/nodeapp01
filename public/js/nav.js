9a607fda6f804568c5ebb6c2554a0f2bd329be57
WeGo1206 <gweissflog@hotmail.com>
2019-01-06 16:22:15 +0100

    New feature: updating of data by using ajax calls

--- public/js/nav.js
+++ public/js/nav.js
@@ -22,4 +22,39 @@ function changeView(i) {
     }
     
     xhttp.send();
+
+    var c;
+    for(c=1; c<=4; c++) {
+
+        elId= c+'-menue-btn';
+        el= document.getElementByClass(elId);
+        el.classList.remove('selected-menue-btn');
+        el.classList.add('deselected-menue-btn');
+        console.log(el);
+    }
+    
+    switch (i) {
+        case 1:
+            el= document.getElementByClass('1-menue-btn');
+            el.classList.remove('deselected-menue-btn');
+            el.classList.add('selected-menue-btn');
+            console.log('case 1');
+            console.log(el);
+          break;
+        case 2:
+            el= document.getElementByClass('2-menue-btn');
+            el.classList.remove('deselected-menue-btn');
+            el.classList.add('selected-menue-btn');
+          break;
+        case 3:
+            el= document.getElementByClass('3-menue-btn');
+            el.classList.remove('deselected-menue-btn');
+            el.classList.add('selected-menue-btn');
+          break;
+        case 4:
+            el= document.getElementByClass('4-menue-btn');
+            el.classList.remove('deselected-menue-btn');
+            el.classList.add('selected-menue-btn');
+    };
+        
 };
